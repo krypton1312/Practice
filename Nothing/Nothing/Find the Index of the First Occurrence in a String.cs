bfe926ba86724d54e48451aba67fb1f57e244abf
kr3pt0n <egor20042013@gmail.com>
2023-08-24 13:35:44 +0200

    study

---
+++ Nothing/Nothing/Find the Index of the First Occurrence in a String.cs
@@ -0,0 +1,26 @@
+﻿global using global::System;
+global using global::System.Collections.Generic;
+global using global::System.IO;
+global using global::System.Linq;
+global using global::System.Net.Http;
+global using global::System.Threading;
+global using global::System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "sadbut";
+        string str2 = "sad";
+
+        int StrStr(string haystack, string needle)
+        {
+
+            return haystack.IndexOf(needle);
+
+        }
+        Console.WriteLine(StrStr(str1, str2));
+    }
+}
---
+++ Nothing/Valid anagram1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Valid_anagram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "anagram";
+            string t = "nagaram";
+            Dictionary<string, int> annagram = new Dictionary<string, int>();
+            for(int i = 0; i <s.Length; i++)
+            {
+                if (annagram.ContainsKey(s.Substring(i,1)))
+                {
+                    annagram[s.Substring(i, 1)]++;
+                }
+                else
+                {
+                    annagram.Add(s.Substring(i, 1), 1);
+                }
+            }
+            
+            Dictionary<string, int> dic2 = new Dictionary<string, int>();
+            for (int j = 0; j < t.Length; j++)
+            {
+                if (dic2.ContainsKey(t.Substring(j, 1)))
+                {
+                    dic2[t.Substring(j, 1)]++;
+                }
+                else
+                {
+                    dic2.Add(t.Substring(j, 1), 1);
+                }
+            }
+            
+            var sortedDict1 = annagram.OrderBy(kvp => kvp.Key);
+            var sortedDict2 = dic2.OrderBy(kvp => kvp.Key);
+            bool areEqual = sortedDict1.SequenceEqual(sortedDict2);
+        }
+    }
+}
