76a27929f3ad2328e7378dc8057b1a30b7031416
kr3pt0n <egor20042013@gmail.com>
2023-08-29 13:27:39 +0200

    something more

---
+++ Nothing/move zeroes/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace move_zeroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 0, 1, 0, 3, 12 };
+            var anInstanceofMyClass = new Program();
+            int[] res1 = anInstanceofMyClass.MoveZeroes(ints);
+            for (int j = 0; j < res1.Length; j++) { Console.Write(res1[j] + " "); }
+        }
+        int[] MoveZeroes(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            int b = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    result[b] = nums[i];
+                    b++;
+                }
+            }
+            for (int i = b; i < result.Length; i++)
+            {
+                result[i] = 0;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Nothing/repeated substring/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace repeated_substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "aba";
+            int a = s.Length;
+            Console.WriteLine(a);
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s.Substring(i, 1)))
+                {
+                    dic[s.Substring(i, 1)]++;
+                }
+                else
+                {
+                    dic.Add(s.Substring(i, 1), 1);
+                }
+            }
+
+            ICollection<int> values = dic.Values;
+            int firstValueCount = values.First();
+
+            bool hasEqualValueCount = values.All(value => value == firstValueCount);
+
+            if (hasEqualValueCount)
+            {
+                Console.WriteLine("Все значения в словаре встречаются одинаковое количество раз.");
+            }
+            else
+            {
+                Console.WriteLine("Значения в словаре имеют разное количество вхождений.");
+            }
+
+        }
+    }
+}
