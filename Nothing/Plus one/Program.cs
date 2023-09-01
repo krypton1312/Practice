5b546991d62fdb16654553c2fdb368b954984294
kr3pt0n <egor20042013@gmail.com>
2023-09-01 13:46:27 +0200

    a little bit more

---
+++ Nothing/Plus one/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Plus_one
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 4, 1, 1, 9};
+            var anInstanceofMyClass = new Program();
+            int[] res1 = anInstanceofMyClass.PlusOne(num);
+            for (int j = 0; j < res1.Length; j++) { Console.Write(res1[j] + " "); }
+
+        }
+        int[] PlusOne(int[] digits)
+        {
+            int n = digits.Length;
+
+            // Переменная, которая отвечает за перенос единицы
+            int carry = 1;
+
+            // Идем с конца массива к началу
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int sum = digits[i] + carry;
+                carry = sum / 10; // Вычисляем перенос
+
+                // Записываем остаток от деления в текущую позицию
+                digits[i] = sum % 10;
+            }
+
+            // Если остался перенос, добавляем новый элемент в начало массива
+            if (carry > 0)
+            {
+                int[] result = new int[n + 1];
+                result[0] = carry;
+                Array.Copy(digits, 0, result, 1, n);
+                return result;
+            }
+
+            return digits;
+        }
+    }
+}
---
+++ Nothing/Sign of the Product of an Array/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Sign_of_the_Product_of_an_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 41, 65, 14, 80, 20, 10, 55, 58, 24, 56, 28, 86, 96, 10, 3, 84, 4, 41, 13, 32, 42, 43, 83, 78, 82, 70, 15, -41 };
+            long res = nums[0];
+            for (int i = 1;i < nums.Length; i++)
+            {
+                res = res * nums[i];
+            }
+            if (res == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (res < 0)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+            Console.WriteLine(res);
+
+        }
+    }
+}
